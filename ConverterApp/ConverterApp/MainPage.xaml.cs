f059c1b19ec4ff3382fe73a067e80193a03b3461
Morten Andersen <mmmmn77@hotmail.com>
2021-02-28 12:30:13 +0100

    not really working now. following tutorial from the net. https://www.c-sharpcorner.com/article/listview-and-creating-list-in-xamarin/

--- ConverterApp/ConverterApp/MainPage.xaml.cs
+++ ConverterApp/ConverterApp/MainPage.xaml.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using ConverterApp.Models;
+using System;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace ConverterApp
+
+
 {
     public partial class MainPage : ContentPage
     {
+        private ObservableCollection<Contacts> myList;
+
+        public ObservableCollection<Contacts> MyList
+        {
+            get { return myList; }
+            set { myList = value; }
+        }
+
         public MainPage()
         {
             InitializeComponent();
+
+            this.BindingContext = this;
+
+            MyList = new ObservableCollection<Contacts>();
+
+            for (int i = 1; i < 10; i++)
+            {
+                MyList.Add(new Contacts() { Id = i, Name = "Student" + i.ToString(), Address = "Address" + i.ToString(), Image = "usa.png" });
+            }
+
+            ContactsList.ItemsSource = MyList;
         }
 
         private void Button_Clicked(object sender, EventArgs e)
